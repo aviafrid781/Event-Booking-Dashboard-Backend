4294c44f5f61b62457ebac0dd6530df0c7275c33
aviafrid781 <avi15-11781@diu.edu.bd>
2026-07-10 12:38:13 +0600

    update booking and event entity time type

--- src/bookings/booking.entity.ts
+++ src/bookings/booking.entity.ts
@@ -62,9 +62,9 @@ export class Booking {
   @Column({ name: 'failure_reason', type: 'varchar', nullable: true })
   failureReason: string | null;
 
-  @CreateDateColumn({ name: 'created_at' })
+  @CreateDateColumn({ name: 'created_at', type: 'timestamptz' })
   createdAt: Date;
 
-  @UpdateDateColumn({ name: 'updated_at' })
+  @UpdateDateColumn({ name: 'updated_at', type: 'timestamptz' })
   updatedAt: Date;
 }
